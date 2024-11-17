3986f9b56902ffe539549912ea55500e9f6025bf
Abdullahi Ahmed <cabdullahi_143@hotmail.com>
2024-11-17 21:19:43 +0100

    updated

--- app/routes/ladda-upp.tsx
+++ app/routes/ladda-upp.tsx
@@ -311,10 +311,7 @@ export default function UploadPage() {
             exit={{ opacity: 0 }}
           >
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-              </div>
-
+             
               <motion.div
                 className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
                 initial={{ y: -50, opacity: 0 }}
