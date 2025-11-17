68a265a873e0d0a0d828e6f878b008e5ffb68dd0
Abbe <abbe@Abbes-MacBook-Air.local>
2025-11-17 12:11:29 +0100

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-runtime/plugin";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -45,7 +44,6 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
-    netlifyPlugin(),
     tsconfigPaths(),
   ],
 });
