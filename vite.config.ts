a7b08aab93d78de5d8f9933693ee1f0119f7a72b
Abbe <abbe@Abbes-MacBook-Air.local>
2025-11-17 12:17:23 +0100

    Configure Remix for Netlify Edge Functions - replace remix-runtime with remix-edge-adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -44,6 +45,7 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    netlifyPlugin(),
     tsconfigPaths(),
   ],
 });
